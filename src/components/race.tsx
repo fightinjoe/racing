a315a5f1136ed844a07f00a955b56241fc866c18
Aaron Wheeler <aaron@localby.co>
2024-08-28 17:44:25 -0400

    Added interactivity to Race.start component

--- src/components/race.tsx
+++ src/components/race.tsx
@@ -1,10 +1,26 @@
-import { NavTile } from "@/components/tile"
+import { useRouter } from "next/navigation"
+import { useRaceStore } from "@/stores/raceStore"
+
+import Tile, { NavTile } from "@/components/tile"
 
 function StartRacePartial({fleet = 'AB'}: {fleet?:FleetSchema}) {
+  const router = useRouter()
+  const startRace = useRaceStore(s=>s.startRace)
+
+  const handleClick = () => {
+    // create the race
+    const race = startRace()
+
+    // redirect to the race
+    router.push(`/races/${race.id}`)
+  }
+
   return (
-    <button>
-      New race for fleet {fleet}
-    </button>
+    <Tile
+      title="Start race"
+      subtitle="Fleet AB"
+      onClick={ handleClick }
+    />
   )
 }
 
