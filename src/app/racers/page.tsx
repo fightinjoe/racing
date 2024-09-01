5ea9ce0dbeb34717350fe112f604ce1f9698c059
Aaron Wheeler <aaron@localby.co>
2024-09-01 17:44:45 -0400

    Added back button to /racers page

--- src/app/racers/page.tsx
+++ src/app/racers/page.tsx
@@ -1,12 +1,21 @@
+'use client'
+
 import ListPartial from "./_list"
 import AddPartial from "./_add"
+import HTML from "@/components/html"
 
 export default function RacersPage() {
   return (
-    <div>
-      <h1 className="font-bold text-xl">Racers</h1>
-      <AddPartial />
-      <ListPartial />
-    </div>
+    <main>
+      <header className="flex flex-row p-4">
+        <HTML.back />
+        <HTML.h1 title="Racers" />
+      </header>
+
+      <div>
+        <AddPartial />
+        <ListPartial />
+      </div>
+    </main>
   )
 }
