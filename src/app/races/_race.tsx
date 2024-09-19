648a78e481933941a00e836fe46c2590f31e9dae
Aaron Wheeler <aaron@localby.co>
2024-09-19 16:22:22 -0400

    Removed unused pages

--- src/app/races/_race.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-
-export default function RacePartial({race}: {race: RaceSchema}) {
-  return (
-    <div>
-      <Link href={`/races/${race.id}`}>Race {race.id}</Link>
-    </div>
-  )
-}
