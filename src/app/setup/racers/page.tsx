aead79b05031ce22148d24243a0ad94151b62065
Aaron Wheeler <aaron@fightinjoe.com>
2024-10-24 11:24:22 -0400

    Minor add racers polish

--- src/app/setup/racers/page.tsx
+++ src/app/setup/racers/page.tsx
@@ -5,6 +5,7 @@ import { useRaceDayStore } from "@/stores/raceDayStore"
 import { useRacerSort } from "@/lib/useRacerSort"
 
 import AddRacer from "./_add"
+import Tile from "@/components/tile"
 import HTML from "@/components/html"
 import Button from "@/components/button"
 import { ModalTile } from "@/components/tile"
@@ -20,7 +21,8 @@ export default function RacersPage() {
 
   const {Tabs, helpSortRacers} = useRacerSort()
 
-  const [showAddForm, setShowAddForm] = useState(false)
+  // Show the form by default when there are no racers
+  const [showAddForm, setShowAddForm] = useState( racers.length === 0 )
   const [racerToEdit, setRacerToEdit] = useState<RacerSchema | null>(null)
 
   const onSave = (data: RacerFormSchema) => {
@@ -36,13 +38,13 @@ export default function RacersPage() {
     // CANCEL button
     if( e.target === addRef.current ) return
 
-    if( formRef.current && !formRef.current.contains(e.target as Node) ) {
+    if( racers.length > 0 && formRef.current && !formRef.current.contains(e.target as Node) ) {
       onCancel()
     }
   }
 
-  const onAdd = () => {
-    if( showAddForm ) return onCancel()
+  const onAdd = (onlyAdd?: boolean) => {
+    if( !onlyAdd && showAddForm ) return onCancel()
       
     console.log('add')
     setShowAddForm(true)
@@ -70,8 +72,8 @@ export default function RacersPage() {
   return (
     <main className="h-full col-0 relative">
       <HTML.BackHeader title="Racers">
-      <button ref={addRef} className="button-header" onClick={onAdd}>
-          {showAddForm ? 'Cancel' : 'Add' }
+        <button ref={addRef} className="button-header" onClick={() => onAdd(true)}>
+          {showAddForm ? '' : 'Add' }
         </button>
       </HTML.BackHeader>
 
@@ -80,32 +82,37 @@ export default function RacersPage() {
       </section>
 
       <section className="p-4 pb-0 col-4 shadow-inner shrink overflow-y-hidden">
-        { racers.length > 0 && <Tabs darkMode={true} /> }
+        <Tabs darkMode={true} />
 
-        <div className="row-wrap-2 pb-4 overflow-scroll">{
-          racers.sort( helpSortRacers ).map( (racer,i) => (
-            <ModalTile
-              key={i}
-              sailor={racer}
-              className={racerToEdit && racer.id === racerToEdit.id ? 'bg-yellow-100' : 'bg-ocean-200'}
-            >
-              <div className="col-0 gap-[1px]">
-                <Button.Primary
-                  onClick={ onEditHandler(racer) }
-                  className={ styles.editButton }
-                >
-                  Edit
-                </Button.Primary>
-                
-                <Button.Secondary
-                  onClick={ () => confirm('Do you want to permanently delete this racer?') && deleteRacer(racer) }
-                >
-                  Delete
-                </Button.Secondary>
-              </div>
-            </ModalTile>
-          ))
-        }<div className="h-4">&nbsp;</div>
+        <div className="row-wrap-2 pb-4 overflow-scroll">
+          {
+            racers.length === 0 &&
+            <Tile className="tile-todo" title="+" subtitle="Add racer" onClick={onAdd} />
+          }
+          {
+            racers.sort( helpSortRacers ).map( (racer,i) => (
+              <ModalTile
+                key={i}
+                sailor={racer}
+                className={racerToEdit && racer.id === racerToEdit.id ? 'bg-yellow-100' : 'bg-ocean-200'}
+              >
+                <div className="col-0 gap-[1px]">
+                  <Button.Primary
+                    onClick={ onEditHandler(racer) }
+                    className={ styles.editButton }
+                  >
+                    Edit
+                  </Button.Primary>
+                  
+                  <Button.Secondary
+                    onClick={ () => confirm('Do you want to permanently delete this racer?') && deleteRacer(racer) }
+                  >
+                    Delete
+                  </Button.Secondary>
+                </div>
+              </ModalTile>
+            ))
+          }
         </div>
 
       </section>
