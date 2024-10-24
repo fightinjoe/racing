bbc77647121cfd86b0f97c324868d9929c5563fd
Aaron Wheeler <aaron@fightinjoe.com>
2024-10-24 09:51:06 -0400

    Added ability to add racers

--- src/app/setup/reset/page.tsx
+++ src/app/setup/reset/page.tsx
@@ -1,28 +1,39 @@
 'use client'
 
 import { useRaceDayStore } from "@/stores/raceDayStore"
+import { RACERS } from "@/schemas/CONFIG"
 
 import Button from "@/components/button"
 
 import HTML from "@/components/html"
 
 export default function ResetPage() {
-  const [races, racers, clearRaces, clearRacers, clearConfig] =
-    useRaceDayStore( s=>[s.races, s.racers, s.clearRaces, s.clearRacers, s.clearConfig])
+  const [races, racers, addRacer, clearRaces, clearRacers, clearConfig] =
+    useRaceDayStore( s=>[s.races, s.racers, s.addRacer, s.clearRaces, s.clearRacers, s.clearConfig])
+
+  const loadRacers = () => {
+    const names = racers.map( r=>r.name )
+
+    RACERS
+      .filter( r => !names.includes(r.name) )
+      .forEach( r => { addRacer( r.name, r.sailNumber, r.fleet ) })
+  }
 
   return (
     <div>
       <HTML.BackHeader title="Clear data" />
 
       <section className="m-2 p-2 rounded-lg  grid grid-cols-2 gap-4 items-center bg-white">
-        <HTML.H1 className="text-center">{ races.length } races</HTML.H1>
+        <HTML.H1 className="text-center !text-black">{ races.length } races</HTML.H1>
         <Button.Primary onClick={ clearRaces }>Clear all races</Button.Primary>
       
-        <HTML.H1 className="text-center">{ racers.length } racers</HTML.H1>
+        <HTML.H1 className="text-center !text-black">{ racers.length } racers</HTML.H1>
         <Button.Primary onClick={ clearRacers }>Clear all racers</Button.Primary>
       
-        <HTML.H1 className="text-center">Race day</HTML.H1>
+        <HTML.H1 className="text-center !text-black">Race day</HTML.H1>
         <Button.Primary onClick={ clearConfig }>Clear details</Button.Primary>
+      
+        <Button.Primary onClick={ loadRacers }>Load racers</Button.Primary>
       </section>
 
     </div>
