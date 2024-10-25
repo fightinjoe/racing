dfb64f68324cf567798f4bd64f0ca17be5ff5ded
Aaron Wheeler <aaron@fightinjoe.com>
2024-10-25 16:04:17 -0400

    Addition of Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import type { Preview } from "@storybook/react";
+import { Inter } from 'next/font/google';
+
+import '../src/app/globals.css';
+
+const inter = Inter({ subsets: ["latin"] })
+
+const preview: Preview = {
+  decorators: [
+    (Story) => <div className={inter.className}><Story /></div>,
+  ],
+
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/components/tile.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Tile from './tile'
+
+const meta = {
+  title: 'MFA/Tile',
+  component: Tile,
+  parameters: {},
+  tags: [],
+  decorators: [
+    (Story) => (
+      <div className="bg-ocean-radial p-4 w-[390px] box-content">
+        <div style={{ 'containerType': 'inline-size' }}>
+          <Story />
+        </div>
+      </div>
+    )
+  ]
+} satisfies Meta<typeof Tile>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Racer: Story = {
+  args: {
+    title: '108',
+    subtitle: 'Aaron Wheeler (A fleet)',
+  }
+}
+
+export const Todo: Story = {
+  args: {
+    title: '+',
+    subtitle: 'Add racers',
+    className: 'tile-todo'
+  }
+}
+
+export const Emphasize: Story = {
+  args: {
+    title: '+',
+    subtitle: 'Add racers',
+    className: 'tile-emphasize'
+  }
+}
+
+export const Done: Story = {
+  args: {
+    title: '31 Racers',
+    subtitle: <><strong>5</strong> A fleet<br /><strong>0</strong> B fleet</>,
+    className: 'tile-done'
+  }
+}
