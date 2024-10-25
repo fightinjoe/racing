f4500f1e0bf273c82d8ee590792f3e368f5da273
Aaron Wheeler <aaron@fightinjoe.com>
2024-10-25 18:13:13 -0400

    Mocked Next.js router for Storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import type { Preview } from "@storybook/react";
 import { Inter } from 'next/font/google';
 
+// Mock Next.js router
+// https://github.com/storybookjs/storybook/issues/24722#issuecomment-2087844754
+import {
+	AppRouterContext,
+	type AppRouterInstance,
+} from 'next/dist/shared/lib/app-router-context.shared-runtime'
+
 import '../src/app/globals.css';
 
 const inter = Inter({ subsets: ["latin"] })
 
 const preview: Preview = {
   decorators: [
+		(Story) => (
+			<AppRouterContext.Provider value={{} as AppRouterInstance}>
+				<Story />
+			</AppRouterContext.Provider>
+		),
     (Story) => <div className={inter.className}><Story /></div>,
   ],
 
