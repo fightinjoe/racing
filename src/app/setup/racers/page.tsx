f0db4cdec7b37adf3af55c545ed34e36faff3a20
Aaron Wheeler <aaron@fightinjoe.com>
2024-11-28 09:31:24 -0500

    Added ability to hide and show the full roster

--- src/app/setup/racers/page.tsx
+++ src/app/setup/racers/page.tsx
@@ -19,9 +19,8 @@ export default function RacersPage() {
   const [racers, addRacer, editRacer, deleteRacer, racerRegistered] =
     useRaceDayStore(s => [s.racers, s.addRacer, s.editRacer, s.deleteRacer, s.racerRegistered])
 
-  const racerLookup = new Map(racers.map(r => [r.name, r]))
-
-  const [roster, fetchRoster] = useRosterStore(s => [s.roster, s.fetchRoster])
+  const roster = useRosterStore(s => s.roster)
+  const [showRoster, setShowRoster] = useState<boolean>(true)
 
   const {Tabs, helpSortRacers} = useRacerSort({sorts: ['name', 'number', 'fleet']})
 
@@ -48,6 +47,55 @@ export default function RacersPage() {
       setRacerToEdit(racer)
     }
   }
+
+  const toggleRoster = () => {
+    setShowRoster( !showRoster )
+  }
+
+  /*== Helpers ==*/
+
+  // Converts a sailor from the roster into a racer for printing
+  // as a tile on the racers page
+  function helpInferRacer(sailor: SailorSchema): RacerSchema {
+    return {
+      ...sailor,
+      role: 'Sailor',
+      isGuest: false,
+      sailNumber: sailor.suggestedSailNumbers?.[0] || '?',
+      fleet: sailor.suggestedFleet as FleetSchema
+    } as RacerSchema
+  }
+
+  function helpMergeRacersAndRoster(): RacerSchema[] {
+    if (!showRoster) return racers
+
+    // A unique list of all the names of racers and roster members. Necessary
+    // because guests that are added as sailors won't be on the roster
+    let names = new Set()
+    let racerLookup = new Map()
+    let rosterLookup = new Map()
+
+    racers.map( r => {
+      names.add(r.name)
+      racerLookup.set(r.name, r)
+    })
+
+    roster.map( r => {
+      names.add(r.name)
+      rosterLookup.set(r.name, helpInferRacer(r) )
+    })
+
+    return Array.from(names)
+      .map( name => racerLookup.get(name) || rosterLookup.get(name) )
+  }
+
+  /*== Partials ==*/
+
+  const _ToggleRoster = () => (
+    <button className="button-header" onClick={toggleRoster}>
+      {showRoster ? 'Hide roster' : 'Show roster' }
+    </button>
+  )
 
   const _RosterModalOptions = ({sailor}:{sailor: SailorSchema}) => (
     <div className="col-0 gap-[1px]">
@@ -89,6 +137,27 @@ export default function RacersPage() {
     </div>
   )
 
+  const _RacerGrid = () => (
+    helpMergeRacersAndRoster()
+    .sort(helpSortRacers)
+    .map( (sailor, i) => {
+      const isRegistered = sailor.role === 'Racer'
+
+      let className = isRegistered ? 'bg-ocean-200' : 'tile-todo !bg-clear-100'
+
+      let TileRenderer: React.FC<DefaultTileProps<RacerSchema>> =
+        isRegistered ? RegisteredRacer : UnregisteredSailor
+
+      return (
+        <ModalTile<RacerSchema> key={i} {...{sailor, className, TileRenderer}}>
+          { isRegistered
+            ? <_RacerModalOptions racer={sailor} />
+            : <_RosterModalOptions sailor={sailor} /> }
+        </ModalTile>
+      )
+    })
+  )
+
   const _AddRacerTile = () => (
     <Tile
       className="tile-todo"
@@ -98,22 +167,10 @@ export default function RacersPage() {
     />
   )
 
-  function inferRacer(sailor: SailorSchema) {
-    return {
-      ...sailor,
-      role: 'Racer',
-      isGuest: false,
-      sailNumber: sailor.suggestedSailNumbers?.[0] || '?',
-      fleet: sailor.suggestedFleet as FleetSchema
-    } as RacerSchema
-  }
-
   return (
     <main className="h-full col-0 relative">
       <HTML.BackHeader title={ `${racers.length} Racer${racers.length === 1 ? '' : 's'}` }>
-        <button className="button-header" onClick={() => fetchRoster()}>
-          {modal.props.visible ? '' : 'Load roster' }
-        </button>
+        <_ToggleRoster />
       </HTML.BackHeader>
 
       {/* ADD RACER modal. May be triggered from the header or a tile */}
@@ -132,38 +189,7 @@ export default function RacersPage() {
         {/* Racers */}
         <div className="row-wrap-2 px-4 pt-2 pb-4 overflow-scroll">
           <_AddRacerTile />
-
-          {
-            roster
-            .map( inferRacer )
-            .sort(helpSortRacers)
-            .map( (member, i) => {
-              const isRegistered = racerLookup.has(member.name)
-              
-              let sailor: RacerSchema = isRegistered
-              ? racerLookup.get(member.name)!
-              : {
-                ...member,
-                role: 'Racer',
-                isGuest: false,
-                sailNumber: member.suggestedSailNumbers?.[0] || '?',
-                fleet: member.suggestedFleet as FleetSchema
-              }
-
-              let className = isRegistered ? 'bg-ocean-200' : 'tile-todo !bg-clear-100'
-
-              let TileRenderer: React.FC<DefaultTileProps<RacerSchema>> =
-                isRegistered ? RegisteredRacer : UnregisteredSailor
-
-              return (
-                <ModalTile<RacerSchema> key={i} {...{sailor, className, TileRenderer}}>
-                  { isRegistered
-                    ? <_RacerModalOptions racer={sailor} />
-                    : <_RosterModalOptions sailor={member} /> }
-                </ModalTile>
-              )
-            })
-          }
+          <_RacerGrid />
         </div>
 
       </section>
